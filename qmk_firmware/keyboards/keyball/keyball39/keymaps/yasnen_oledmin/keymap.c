5310faff1197d9ca2985a9062b63cc50333ce4a3
Yasnen <mryas.jp@gmail.com>
2026-03-01 01:34:54 +0900

    update yasnen's keymap

---
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yasnen_oledmin/keymap.c
@@ -0,0 +1,123 @@
+
+/*
+Copyright 2026 @Yasnen (yasnen)
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#include "quantum.h"
+#include "keymap_japanese.h"
+
+// 例: Q+W を ESC、Z+X を マウスボタン1、V+B を マウスボタン2、O+P を BSPC にする
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM as_combo[] = {KC_A, KC_S, COMBO_END};
+//const uint16_t PROGMEM op_combo[] = {KC_O, KC_P, COMBO_END};
+//const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
+//const uint16_t PROGMEM vb_combo[] = {KC_V, KC_B, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(qw_combo, KC_ESC),
+  COMBO(as_combo, KC_TAB),
+//  COMBO(op_combo, KC_BSPC),
+//  COMBO(xc_combo, KC_BTN1),
+//  COMBO(vb_combo, KC_BTN2),
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // keymap for default
+  [0] = LAYOUT_universal(
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S    ,CTL_T(KC_D),SFT_T(KC_F), KC_G    ,                            KC_H    ,SFT_T(KC_J),CTL_T(KC_K),KC_L     , KC_SCLN  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    TO(3)    , KC_1     , KC_LGUI  , KC_RALT  , MO(1)    , KC_SPC,           KC_ENT , MO(2)    , _______  , _______  , _______  , KC_RCTL
+  ),
+
+  [1] = LAYOUT_universal(
+    _______  , KC_UP    , KC_F2    , KC_PGUP  , KC_INS   ,                            JP_KANA  , JP_CIRC  , KC_TAB   , JP_MINS  , JP_SLSH  ,
+    KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_ENT   , KC_DEL   ,                            KC_BSPC  , JP_AT    , JP_LBRC  , JP_RBRC  , JP_COLN ,
+    KC_BTN3  , KC_BTN1  , KC_BTN2  , KC_PGDN  , KC_BSPC  ,                            JP_HENK  , JP_MHEN  , KC_HOME  , KC_END   , JP_BSLS  ,
+    _______  , SCRL_MO  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [2] = LAYOUT_universal(
+    KC_F11   , KC_F12   , KC_F3    , KC_F4    , KC_F5    ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     ,
+    S(KC_1)  , S(KC_2)  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                            S(KC_6)  , S(KC_7)  , S(KC_8)  , S(KC_9)  , S(KC_0)  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , _______
+  ),
+
+  [3] = LAYOUT_universal(
+    _______  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            SCRL_DVD , SCRL_DVI , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    _______  , _______  , _______  , _______  , _______  ,                            _______  , _______  , _______  , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  ,                            _______  , CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  ,
+    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , _______  , KBC_SAVE
+  ),
+  
+/*
+  // keymap for default
+  [0] = LAYOUT_universal(
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
+    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+  ),
+
+  [1] = LAYOUT_universal(
+    KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_RBRC  ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,
+    KC_F5    , KC_EXLM  , S(KC_6)  ,S(KC_INT3), S(KC_8)  ,                           S(KC_INT1), KC_BTN1  , KC_PGUP  , KC_BTN2  , KC_SCLN  ,
+    S(KC_EQL),S(KC_LBRC),S(KC_7)   , S(KC_2)  ,S(KC_RBRC),                            KC_LBRC  , KC_DLR   , KC_PGDN  , KC_BTN3  , KC_F11   ,
+    KC_INT1  , KC_EQL   , S(KC_3)  , _______  , _______  , _______  ,      TO(2)    , TO(0)    , _______  , KC_RALT  , KC_RGUI  , KC_F12
+  ),
+
+  [2] = LAYOUT_universal(
+    KC_TAB   , KC_7     , KC_8     , KC_9     , KC_MINS  ,                            KC_NUHS  , _______  , KC_BTN3  , _______  , KC_BSPC  ,
+   S(KC_QUOT), KC_4     , KC_5     , KC_6     ,S(KC_SCLN),                            S(KC_9)  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_QUOT  ,
+    KC_SLSH  , KC_1     , KC_2     , KC_3     ,S(KC_MINS),                           S(KC_NUHS), KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+    KC_ESC   , KC_0     , KC_DOT   , KC_DEL   , KC_ENT   , KC_BSPC  ,      _______  , _______  , _______  , _______  , _______  , _______
+  ),
+  
+  [3] = LAYOUT_universal(
+    RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , _______  ,                            _______  , _______  , SSNP_HOR , SSNP_VRT , SSNP_FRE ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , SCRL_DVI ,                            _______  , _______  , _______  , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , SCRL_DVD ,                            CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , KBC_SAVE ,
+    QK_BOOT  , KBC_RST  , _______  , _______  , _______  , _______  ,      _______  , _______  , _______  , _______  , KBC_RST  , QK_BOOT
+  ),
+*/
+};
+// clang-format on
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Auto enable scroll mode when the highest layer is 3
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    return state;
+}
+
+#ifdef OLED_ENABLE
+
+#    include "lib/oledkit/oledkit.h"
+
+void oledkit_render_info_user(void) {
+    keyball_oled_render_layerinfo();
+}
+#endif
+
+// キーマップ
+// quantum/keycodes.h
+// quantum/keymap_extras/keymap_japanese.h
+// 
---
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/yasnen_oledmin/rules.mk
@@ -0,0 +1,11 @@
+# Enabled only one of RGBLIGHT and RGB_MATRIX if necessary.
+RGBLIGHT_ENABLE = yes
+
+# To support OLED
+OLED_ENABLE = yes
+
+# TRI_LAYER_ENABLE = no
+
+# VIA_ENABLE = yes
+
+COMBO_ENABLE = yes
